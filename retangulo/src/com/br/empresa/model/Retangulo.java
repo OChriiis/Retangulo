ccfc9482fbaf6c7a8f4a1a2782fb6db140ad9361
HERICK ANDERSON DOS SANTOS <20272523@cfp127aluno.local>
2021-01-27 11:39:22 -0300

    retangulo

---
+++ retangulo/src/com/br/empresa/model/Retangulo.java
@@ -0,0 +1,31 @@
+package com.br.empresa.model;
+
+public class Retangulo {
+
+	private double altura;
+	private double base;
+	
+	public void setAltura(double altura) {
+		this.altura = altura;
+	}
+	
+	public double getAltura () {
+		return this.altura;
+	}
+	
+	public void setBase(double base) {
+		this.base = base;
+	}
+	
+	public double getBase() {
+		return base;
+	}
+	
+	public double calcularArea() {
+		return this.base * this.altura;
+	}
+	
+	public double calcularPerimetro () {
+		return 2 * (this.base + this.altura);
+	}
+} 
